d8d619baaf074b79e3ca2a5970d2445bd1dbd676
Lynh <jenky.w0w@gmail.com>
2023-07-22 22:12:15 +0700

    Rename exceptions

--- tests/PoolTest.php
+++ tests/PoolTest.php
@@ -3,7 +3,7 @@
 namespace Jenky\Atlas\Pool\Tests;
 
 use Jenky\Atlas\NullConnector;
-use Jenky\Atlas\Pool\Exceptions\UnsupportedException;
+use Jenky\Atlas\Pool\Exceptions\UnsupportedFeatureException;
 use Jenky\Atlas\Pool\PoolFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -37,7 +37,7 @@ final class PoolTest extends TestCase
     public function test_factory(): void
     {
         $this->markTestSkipped();
-        $this->expectException(UnsupportedException::class);
+        $this->expectException(UnsupportedFeatureException::class);
 
         PoolFactory::create(new NullConnector());
     }
