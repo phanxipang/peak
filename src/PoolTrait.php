d635fde552a09bf284ceedd8d73b50216ccd86b0
Lynh <jenky.w0w@gmail.com>
2023-07-25 16:56:04 +0700

    Bug fixes

---
+++ src/PoolTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jenky\Atlas\Pool;
+
+use Jenky\Concurrency\PoolInterface;
+
+trait PoolTrait
+{
+    private int $concurrency = 25;
+
+    public function concurrent(int $concurrency): PoolInterface
+    {
+        if ($concurrency < 1) {
+            throw new \ValueError('Argument #1 ($concurrency) must be positive, got '.$concurrency);
+        }
+
+        $clone = clone $this;
+        $clone->concurrency = $concurrency;
+
+        return $clone;
+    }
+}
